85ff456296e184a6b625c2debaf29c849ed4e1b7
tanmay-deev <tanmaybonde10@gmail.com>
2026-07-23 22:34:14 +0530

    Fixed all the bugs

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../store/authStore.js';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 });
 
 apiClient.interceptors.request.use((config) => {
